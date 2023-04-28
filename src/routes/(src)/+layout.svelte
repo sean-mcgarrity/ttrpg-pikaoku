adbc65bb7978a6b9dc675e8eecc3809728b373a3
sean.mcgarrity <sean@tinpotteam.com>
2023-04-28 12:03:38 +0100

    refactor: layout for newspapers

---
+++ src/routes/(src)/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Header from '$components/Header.svelte';
+  import Footer from '$components/Footer.svelte';
+  import { onMount } from 'svelte';
+  onMount(async () => {});
+</script>
+
+<div
+  class="bg-gray-800 bg-gradient-to-b from-gray-800 to-gray-900 min-h-screen flex flex-col font-sans"
+>
+  <Header />
+  <main class="flex-1 border-0 p-2 sm:p-8">
+    <slot />
+  </main>
+  <Footer />
+</div>
---
+++ src/routes/papers/+layout.svelte
@@ -0,0 +1,3 @@
+<div class="p-4 bg-gray-900">
+  <slot />
+</div>
