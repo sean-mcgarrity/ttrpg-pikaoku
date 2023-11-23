5b00fe003552dee1b19f2bb89c83c0ecbbb40444
Sean McGarrity <pikaoku@gmail.com>
2023-11-23 19:19:56 +0000

    chore: add copyrigth symbol

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,3 +1,4 @@
 <div class="flex w-full flex-col items-center justify-center py-8 px-4">
-  <div class="flex items-center text-white">Sean 2023</div>
+  <div class="flex items-center text-white">&#169; Sean 2023</div>
+  
 </div>
