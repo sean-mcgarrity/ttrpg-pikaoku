accfa7405d067ccae573b243c8e6ef4802d5cfc1
Sean McGarrity <pikaoku@gmail.com>
2023-11-25 00:46:38 +0000

    refactor: Heading  without margin

--- src/components/layout/Heading.svelte
+++ src/components/layout/Heading.svelte
@@ -1,21 +1,24 @@
 <script lang="ts">
+  import cs from 'classnames';
+
   type HeadingTypes = 'Page Heading' | 'Section Heading' | 'Subsection Heading';
 
   export let type: HeadingTypes;
+  export let tight = false;
 </script>
 
 {#if !type}
   NOT ENOUGH INFO
 {:else if type === 'Page Heading'}
-  <h1 class="text-4xl font-semibold mb-4 font-title uppercase">
+  <h1 class={cs(!tight && 'mb-4', 'text-4xl font-semibold font-title uppercase')}>
     <slot />
   </h1>
 {:else if type === 'Section Heading'}
-  <h2 class="text-2xl font-medium mb-2 font-title">
+  <h2 class={cs(!tight && 'mb-2', 'text-2xl font-medium font-title')}>
     <slot />
   </h2>
 {:else if type === 'Subsection Heading'}
-  <h3 class="text-2xl font-bold mb-2 font-title">
+  <h3 class={cs(!tight && 'mb-2', 'text-2xl font-bold font-title')}>
     <slot />
   </h3>
 {/if}
